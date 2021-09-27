00101a5418d43e84a1850eea2be7858180d923cc
Mathieu <mathieu.simon2012@gmail.com>
2021-09-27 09:25:32 +0200

    Diverse files

---
+++ 02_Data/05_hFE/02_FEA/432_L_77_F/Optimization/run_odb_FZ_MAX.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/var/DassaultSystemes/SIMULIA/Commands/abq2021 python /home/mathieu/Documents/FRACTIB/02_Data/05_hFE/03_Scripts/readODB_acc.py C0003103_FZ_MAX_00 /home/mathieu/Documents/FRACTIB/02_Data/05_hFE/02_FEA/432_L_77_F/ LE -1
