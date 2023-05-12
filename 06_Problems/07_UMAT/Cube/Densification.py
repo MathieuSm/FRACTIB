4d3b7bd311e4c2de00787291b1c7459b881cc13b
MathieuWindows <mathieu.simon2012@gmail.com>
2023-05-12 14:04:02 +0200

    Add yield criterion computation

--- 06_Problems/07_UMAT/Cube/Densification.py
+++ 06_Problems/07_UMAT/Cube/Densification.py
@@ -70,6 +70,22 @@ def PlotCube(Nodes, R, Values, FName):
 
     return
 
+def MATMUL(A66, B6i):
+
+    B66 = np.zeros((6,6,B6i.shape[1]))
+    B66[0,0] = B6i[0]
+    B66[1,1] = B6i[1]
+    B66[2,2] = B6i[2]
+    B66[0,1] = B6i[3]
+    B66[0,2] = B6i[4]
+    B66[1,2] = B6i[5]
+    B66[1,0] = B66[0,1]
+    B66[0,2] = B66[2,0]
+    B66[2,1] = B66[1,2]
+
+    C66 = np.matmul(A66, B66)
+
+    return C66
 
 #%% Main
 # Main code
@@ -80,13 +96,30 @@ def Main():
     Nodes = pd.read_csv('Nodes.csv')
     Elements = pd.read_csv('Elements.csv')
 
+    # Input file values
+    MM1 = 1.0
+    MM2 = 1.0
+    MM3 = 1.0
+    PBVT = 1.0
+
     # UMAT values
-    Elim = 0.009
     SCA1 = 0.9419
     SCA2 = 0.7801
+
+    # Elasticity parameters (FABRGWTB PMUBC, PHZ, 2013) new version 2017 trabecular bone
     E0  = 9759.0*(12000.0/9759.0)*SCA1
+    V0  = 0.2278
+    MU0 = 3117.0*(12000.0/9759.0)*SCA1
+    KS  = 1.91
+    LS  = 1.1
+
+    # Strength parameters (FABRGWTB, PHZ, 2013) trabecular bone
     SIGD0N = 73.1*(12000.0/9759.0)*SCA2
     SIGD0P = 57.69*(12000.0/9759.0)*SCA2
+    ZETA0  = 0.28
+    TAUD0  = 29.61*(12000.0/9759.0)*SCA2
+    PP     = 1.82
+    QQ     = 0.98
 
     # Densification
     ECRIT = -0.3
@@ -109,21 +142,64 @@ def Main():
         for K2 in range(3):
             SDNST[:,K1,K2] = (GAMMAL*CRITERION+GAMMAP* CRITERION**(POWERDNS-1.0))*XIDEN[K1,K2]
 
-    # Elastic trial stress
-    STR = (1.0D0-DAM(KAPPA0,KONSTD,CRITD))*MATMUL(SSSS,ETOT1-EPLAS0)
+    # Stiffness tensor SSSS
+    SSSST = np.zeros((6,6))
+    SSSST[0,0] = E0*(1.0-V0)/(1.0+V0)/(1.0-2.0*V0)*MM1**(2.0*LS)*(BVTV**KS)
+    SSSST[1,1] = E0*(1.0-V0)/(1.0+V0)/(1.0-2.0*V0)*MM2**(2.0*LS)*(BVTV**KS)
+    SSSST[2,2] = E0*(1.0-V0)/(1.0+V0)/(1.0-2.0*V0)*MM3**(2.0*LS)*(BVTV**KS)
+    SSSST[3,3] = 2.0*MU0*(MM1**LS)*(MM2**LS)*(BVTV**KS)
+    SSSST[4,4] = 2.0*MU0*(MM3**LS)*(MM1**LS)*(BVTV**KS)
+    SSSST[5,5] = 2.0*MU0*(MM2**LS)*(MM3**LS)*(BVTV**KS)
+    SSSST[1,0] = E0*V0/(1.0+V0)/(1.0-2.0*V0)*(MM1**LS)*(MM2**LS)*(BVTV**KS)
+    SSSST[2,0] = E0*V0/(1.0+V0)/(1.0-2.0*V0)*(MM3**LS)*(MM1**LS)*(BVTV**KS)
+    SSSST[2,1] = E0*V0/(1.0+V0)/(1.0-2.0*V0)*(MM2**LS)*(MM3**LS)*(BVTV**KS)
+    SSSST[0,1] = SSSST[1,0]
+    SSSST[0,2] = SSSST[2,0]
+    SSSST[1,2] = SSSST[2,1]
+    SSSS = SSSST*PBVT
 
-    # Transversly isotropic quadratic second order tensor FF
-    FFC = np.zeros((3,3))
-    FFC[1,1] = -(SIGD0P-SIGD0N)/2.0/SIGD0P/SIGD0N
-    FFC[2,2] = -(SIGD0P-SIGD0N)/2.0/SIGD0P/SIGD0N
-    FFC[3,3] = -(SIGDAP-SIGDAN)/2.0/SIGDAP/SIGDAN
-    FFC    = FFC/BVTV**PP
+    # Elastic trial stress
+    STR = np.zeros((6,len(Elements)))
+    for i in range(6):
+        STR[i] = Elements['S' + str(i+1)]
+
+    # Quadric fourth order tensor FFFF
+    S0 = (SIGD0P+SIGD0N)/2.0/SIGD0P/SIGD0N
+    FFFFT = np.zeros((6,6))
+    FFFFT[0,0] = S0**2/((BVTV**PP)*MM1**(2.0*QQ))**2
+    FFFFT[1,1] = S0**2/((BVTV**PP)*MM2**(2.0*QQ))**2
+    FFFFT[2,2] = S0**2/((BVTV**PP)*MM3**(2.0*QQ))**2
+    FFFFT[0,1] = -(ZETA0*(MM1/MM2)**(2.0*QQ))*S0**2/((BVTV**PP)*MM1**(2.0*QQ))**2
+    FFFFT[0,2] = -(ZETA0*(MM1/MM3)**(2.0*QQ))*S0**2/((BVTV**PP)*MM1**(2.0*QQ))**2
+    FFFFT[1,2] = -(ZETA0*(MM2/MM3)**(2.0*QQ))*S0**2/((BVTV**PP)*MM2**(2.0*QQ))**2
+    FFFFT[1,0] = FFFFT[0,1]
+    FFFFT[2,0] = FFFFT[0,2]
+    FFFFT[2,1] = FFFFT[1,2]
+    FFFFT[3,3] = 0.5/((TAUD0*(BVTV**PP)*(MM1*MM2)**QQ)**2)
+    FFFFT[4,4] = 0.5/((TAUD0*(BVTV**PP)*(MM1*MM3)**QQ)**2)
+    FFFFT[5,5] = 0.5/((TAUD0*(BVTV**PP)*(MM2*MM3)**QQ)**2)
+    FFFF = FFFFT*PBVT
+
+    # Quadratic second order tensor FF
+    FFT = np.zeros((3,3))
+    FFT[0,0] = -(SIGD0P-SIGD0N)/2.0/SIGD0P/SIGD0N/((BVTV**PP)*MM1**(2.0*QQ))
+    FFT[1,1] = -(SIGD0P-SIGD0N)/2.0/SIGD0P/SIGD0N/((BVTV**PP)*MM2**(2.0*QQ))
+    FFT[2,2] = -(SIGD0P-SIGD0N)/2.0/SIGD0P/SIGD0N/((BVTV**PP)*MM3**(2.0*QQ))
+    FFT  = FFT*PBVT
+
+    FF = np.zeros(6)
+    FF[0] = FFT[0,0]
+    FF[1] = FFT[1,1]
+    FF[2] = FFT[2,2]
+    FF[3] = 0.0
+    FF[4] = 0.0
+    FF[5] = 0.0
 
     # Yield criterion
-    RAD  = RADK(PYFL,KSLOPE,KMAX,KWIDTH,RDY,KAPPA0,KMIN,GMIN,ND,EXPS)
+    RAD  = 1.0 # Perfect plasticity
     FFS  = MATMUL(FFFF,STR)
-    SFFS = DOT_PRODUCT(STR,FFS)
-    YSTR = DSQRT(SFFS)+DOT_PRODUCT(FF,STR)-RAD
+    SFFS = np.dot(STR[:,10],FFS[:,:,10])
+    YSTR = np.sqrt(SFFS)+np.dot(FF,STR)-RAD
 
     Figure, Axis = plt.subplots(1,1)
     Axis.plot(Elements['E3'], Elements['S3'], color=(1,0,0), linewidth=2, label='Abaqus Results')
