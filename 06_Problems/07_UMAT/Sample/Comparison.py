2ee68cc43a430d323ce393107cadfa74ccaadd9c
MathieuMSB <mathieu.simon@unibe.ch>
2023-05-16 15:32:19 +0200

    Latest results

--- 06_Problems/07_UMAT/Sample/Comparison.py
+++ 06_Problems/07_UMAT/Sample/Comparison.py
@@ -24,8 +24,11 @@ import argparse
 import numpy as np
 import pandas as pd
 from numba import njit
+from skimage import io
+import SimpleITK as sitk
 import scipy.signal as sig
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 #%% Functions
 # Define functions
@@ -138,6 +141,97 @@ def DecomposeJacobian(JacobianArray):
 
     return SC, ID
 
+def ShowSC(Image, IRange, Slice=None, Title=None, Axis='Z', FName=None):
+
+    try:
+        Array = sitk.GetArrayFromImage(Image)
+        Dimension = Image.GetDimension()
+    except:
+        Array = Image
+        Dimension = len(Array.shape)
+
+    if Dimension == 3:
+        
+        if Axis == 'Z':
+            if Slice:
+                Array = Array[Slice,:,:]
+            else:
+                Array = Array[Array.shape[0]//2,:,:]
+        if Axis == 'Y':
+            if Slice:
+                Array = Array[:,Slice,:]
+            else:
+                Array = Array[:,Array.shape[1]//2,:]
+        if Axis == 'X':
+            if Slice:
+                Array = Array[:,:,Slice]
+            else:
+                Array = Array[:,:,Array.shape[2]//2]
+
+    Figure, Axis = plt.subplots()
+    CMap = Axis.imshow(Array,interpolation=None, cmap='jet', vmin=IRange[0], vmax=IRange[1])
+    CBar = plt.colorbar(CMap, shrink=0.85, ticks=[IRange[0], 1, IRange[1]])
+    Axis.axis('Off')
+    
+    if (Title):
+        Axis.set_title(Title)
+
+    if (FName):
+        plt.savefig(FName, bbox_inches='tight', pad_inches=0)
+
+    plt.close(Figure)
+
+    return
+
+def ShowID(Image, IRange, Slice=None, Title=None, Axis='Z', FName=None):
+
+    N = 256
+    Values = np.zeros((N, 4))
+    Values[:, 0] = np.linspace(0, 1, N)
+    Values[:, 2] = np.linspace(1, 0, N)
+    Values[:, -1] = 1.0
+    CMP = ListedColormap(Values)
+
+    try:
+        Array = sitk.GetArrayFromImage(Image)
+        Dimension = Image.GetDimension()
+    except:
+        Array = Image
+        Dimension = len(Array.shape)
+
+    if Dimension == 3:
+        
+        if Axis == 'Z':
+            if Slice:
+                Array = Array[Slice,:,:]
+            else:
+                Array = Array[Array.shape[0]//2,:,:]
+        if Axis == 'Y':
+            if Slice:
+                Array = Array[:,Slice,:]
+            else:
+                Array = Array[:,Array.shape[1]//2,:]
+        if Axis == 'X':
+            if Slice:
+                Array = Array[:,:,Slice]
+            else:
+                Array = Array[:,:,Array.shape[2]//2]
+
+    Figure, Axis = plt.subplots()
+    CMap = Axis.imshow(Array,interpolation=None, cmap=CMP, vmin=IRange[0], vmax=IRange[1])
+    CBar = plt.colorbar(CMap, shrink=0.85, ticks=[IRange[0], IRange[1]])
+    Axis.axis('Off')
+    
+    if (Title):
+        Axis.set_title(Title)
+
+    if (FName):
+        plt.savefig(FName, bbox_inches='tight', pad_inches=0)
+
+    plt.close(Figure)
+
+    return
+
 
 #%% Main
 # Main code
@@ -182,56 +276,75 @@ def Main():
     plt.show()
 
     # Compare deformation fields
-    Columns = ['X','Y','Z','F11','F12','F13','F21','F22','F23','F31','F32','F33']
-    ElementsDG = pd.read_csv('Elements_DG.csv', names=Columns)
+    Fs = ['F11','F12','F13','F21','F22','F23','F31','F32','F33']
+    ElementsDG = pd.read_csv('PPD_NL_DG.csv')
+
+    Coords = ElementsDG.drop_duplicates(subset=['X', 'Y', 'Z'])
+    X = np.unique(Coords['X'].values)
+    Y = np.unique(Coords['Y'].values)
+    Z = np.unique(Coords['Z'].values)
 
     # Build arrays
-    X = np.unique(ElementsDG['X'].values)
-    Y = np.unique(ElementsDG['Y'].values)
-    Z = np.unique(ElementsDG['Z'].values)
+    Frames = ElementsDG.groupby(by=['Step','Frame'])
+    SC, ID, SF = [], [], []
+    i = 0
+    for (Step, Frame), Data in Frames:
+        F = np.zeros((len(Z),len(Y),len(X),9))
+        F[:,:,:,0] = 1.0
+        F[:,:,:,4] = 1.0
+        F[:,:,:,8] = 1.0
+        for Index in Data.index:
+            
+            Element = Data.loc[Index]
+            X_Index = list(X).index(Element['X'])
+            Y_Index = list(Y).index(Element['Y'])
+            Z_Index = list(Z).index(Element['Z'])
+            
+            F[Z_Index,Y_Index,X_Index] = Element[Fs].values
 
-    F = np.zeros((len(Z),len(Y),len(X),9))
-    for Index in ElementsDG.index:
+        # Decompose deformation
+        SphericalCompression, IsovolumicDeformation = DecomposeJacobian(F)
+        SC.append(SphericalCompression)
+        ID.append(IsovolumicDeformation)
+        SF.append((Step, Frame))
+    SC = np.array(SC)
+    ID = np.array(ID)
+
+    Mask = SC[-1] == 1.0
+    for i, sf in enumerate(SF):
+        SC[i,Mask] = np.nan
+        if i == 0:
+            SC[i,~Mask] = 1.0
+        ShowSC(SC[i], [0, 2], Axis='X', FName='SC_%02d'%i)
+        # ShowID(ID[i], [1.75,3.5], Axis='X', FName='ID_%02d'%i)
+
+        Data = pd.read_csv('PPD_NL_RefNode.csv')
+        F1 = Data['Step'] == sf[0]
+        F2 = Data['Frame'] == sf[1]
+        Index = Data[F1 & F2].index[0]
+
+        Figure, Axis = plt.subplots(1,1, figsize=(4.55, 3.28))
+        Axis.plot(Data.loc[:Index,'Uz'], Data.loc[:Index,'Fz']/1E3, color=(1,0,0))
+        Axis.plot(Data.loc[Index,'Uz'], Data.loc[Index,'Fz']/1E3, color=(1,0,0), marker='o')
+        Axis.set_xlabel('Displacement (mm)')
+        Axis.set_ylabel('Force (kN)')
+        Axis.set_xlim([-0.05, Data['Uz'].max()*1.05])
+        Axis.set_ylim([Data['Fz'].min()*1.05/1E3, Data['Fz'].max()*1.05/1E3])
+        plt.subplots_adjust(left=0.15, bottom=0.15)
+        plt.savefig('RN_%02d'%i, dpi=100)
+        plt.close(Figure)
+
+        Image1 = io.imread('SC_%02d'%i + '.png')
+        Image2 = io.imread('RN_%02d'%i + '.png')
+
+        Shape = (Image1.shape[0], Image1.shape[1] + Image2.shape[1], Image1.shape[2])
+        Image = np.zeros(Shape, 'uint8')
+        Image[:,:Image1.shape[1]] = Image1
+        Image[:,Image1.shape[1]:] = Image2
         
-
-
-    Figure, Axis = plt.subplots(1,1)
-    Axis.plot(Exp['Z'], -Exp['FZ']/1E3, color=(0,0,0), label='Experiment')
-    for i, P in enumerate(PS):
-        for j, L in enumerate(NL):
-            if len(L) > 0:
-                Label = P + ' ' + L
-                Data = pd.read_csv(P + '_' + L + '_RefNode.csv')
-            else:
-                Label = P
-                Data = pd.read_csv(P + '_RefNode.csv')
-            Axis.plot(Data['Uz'], Data['Fz']/1E3, linestyle=Style[j], color=Colors[i], label=Label)
-    Axis.set_xlabel('Displacement (mm)')
-    Axis.set_ylabel('Force (kN)')
-    plt.legend(loc='upper center', ncol=3, bbox_to_anchor=(0.5, 1.1))
-    plt.show()
-
-    # Compare deformation fields
-    Columns = ['X','Y','Z','F11','F12','F13','F21','F22','F23','F31','F32','F33']
-    ElementsDG = pd.read_csv('Elements_DG.csv', names=Columns)
-
-    # Build arrays
-    X = np.unique(ElementsDG['X'].values)
-    Y = np.unique(ElementsDG['Y'].values)
-    Z = np.unique(ElementsDG['Z'].values)
-
-    F = np.zeros((len(Z),len(Y),len(X),9))
-    for Index in ElementsDG.index:
-        
-        Element = ElementsDG.loc[Index]
-        X_Index = list(X).index(Element['X'])
-        Y_Index = list(Y).index(Element['Y'])
-        Z_Index = list(Z).index(Element['Z'])
-        
-        F[Z_Index,Y_Index,X_Index] = Element[Columns[3:]].values
-
-    # Decompose deformation
-    SphericalCompression, IsovolumicDeformation = DecomposeJacobian(F)
+        io.imsave('IM_%02d'%i + '.png', Image)
+        os.remove('SC_%02d'%i + '.png')
+        os.remove('RN_%02d'%i + '.png')
 
     return
 
--- 06_Problems/07_UMAT/Sample/ReadOdbDG.py
+++ 06_Problems/07_UMAT/Sample/ReadOdbDG.py
@@ -7,7 +7,7 @@ import numpy as np
 from odbAccess import *
 
 #print 'Open odb file'
-
+NFrames = sys.argv[2]
 Odb = openOdb(sys.argv[1])
 
 # Create variable refering to model instance
@@ -27,51 +27,53 @@ UFList = []
 Steps = Odb.steps.keys()
 for S, Step in enumerate(Steps):
     Frames = Odb.steps[Step].frames
-    for F in range(len(Frames)):
+    if len(Frames) > 0:
+        for F in np.round(np.linspace(0,len(Frames)-1,NFrames)).astype(int):
 
-        # Get fields output
-        F11 = Frames[F].fieldOutputs['SDV_F11']
-        F12 = Frames[F].fieldOutputs['SDV_F12']
-        F13 = Frames[F].fieldOutputs['SDV_F13']
-        F21 = Frames[F].fieldOutputs['SDV_F21']
-        F22 = Frames[F].fieldOutputs['SDV_F22']
-        F23 = Frames[F].fieldOutputs['SDV_F23']
-        F31 = Frames[F].fieldOutputs['SDV_F31']
-        F32 = Frames[F].fieldOutputs['SDV_F32']
-        F33 = Frames[F].fieldOutputs['SDV_F33']
-        DG = [F11,F12,F13,F21,F22,F23,F31,F32,F33]
+            # Get fields output
+            F11 = Frames[F].fieldOutputs['SDV_F11']
+            F12 = Frames[F].fieldOutputs['SDV_F12']
+            F13 = Frames[F].fieldOutputs['SDV_F13']
+            F21 = Frames[F].fieldOutputs['SDV_F21']
+            F22 = Frames[F].fieldOutputs['SDV_F22']
+            F23 = Frames[F].fieldOutputs['SDV_F23']
+            F31 = Frames[F].fieldOutputs['SDV_F31']
+            F32 = Frames[F].fieldOutputs['SDV_F32']
+            F33 = Frames[F].fieldOutputs['SDV_F33']
+            DG = [F11,F12,F13,F21,F22,F23,F31,F32,F33]
 
-        # Get elements deformation gradient
-        ElementsDG = np.zeros((N,14))
-        for iElement, Element in enumerate(Instance.elements[:N]):
+            # Get elements deformation gradient
+            ElementsDG = np.zeros((N,22))
+            for iElement, Element in enumerate(Instance.elements[:N]):
 
-            Line = "Step " + str(S+1) + "/" + str(len(Steps))
-            Line +=  " Frame " + str(F+1) + "/" + str(len(Frames))
-            Line +=  " Element " + str(iElement+1) + "/" + str(N)
-            sys.stdout.write("\r" + Line + "          ")
-            sys.stdout.flush()
+                Line = "Step " + str(S+1) + "/" + str(len(Steps))
+                Line +=  " Frame " + str(F+1) + "/" + str(len(Frames))
+                Line +=  " Element " + str(iElement+1) + "/" + str(N)
+                sys.stdout.write("\r" + Line + "          ")
+                sys.stdout.flush()
 
-            # Compute element central position
-            XYZ = np.zeros((len(Element.connectivity),3))
-            NodeNumber = 0
-            for NodeLabel in Element.connectivity:
-                Node = np.where(NodeLabels==NodeLabel)[0][0]
-                for Axis in range(3):
-                    XYZ[NodeNumber,Axis] = Nodes[Node].coordinates[Axis]
-                NodeNumber += 1
+                # Compute element central position
+                XYZ = np.zeros((len(Element.connectivity),3))
+                NodeNumber = 0
+                for NodeLabel in Element.connectivity:
+                    Node = np.where(NodeLabels==NodeLabel)[0][0]
+                    for Axis in range(3):
+                        XYZ[NodeNumber,Axis] = Nodes[Node].coordinates[Axis]
+                    NodeNumber += 1
 
-            # Get element mean deformation gradient
-            F_IntegrationPoints = np.zeros((1,9))
-            for F_Component in range(9):
-                F_Value = DG[F_Component].getSubset(region=Element).values[0].data
-                F_IntegrationPoints[0,F_Component] = F_Value
+                # Get element mean deformation gradient
+                F_IntegrationPoints = np.zeros((1,9))
+                for F_Component in range(9):
+                    F_Value = DG[F_Component].getSubset(region=Element).values[0].data
+                    F_IntegrationPoints[0,F_Component] = F_Value
 
-            # Add data to array
-            ElementsDG[iElement,0] = S
-            ElementsDG[iElement,1] = F
-            ElementsDG[iElement,2:5] = np.mean(XYZ,axis=0)
-            for i in range(9):
-                ElementsDG[iElement,5+i] = DG[i].getSubset(region=Element).values[0].data
+                # Add data to array
+                ElementsDG[iElement,0] = S
+                ElementsDG[iElement,1] = F
+                ElementsDG[iElement,2:10] = Element.connectivity
+                ElementsDG[iElement,10:13] = np.mean(XYZ,axis=0)
+                for i in range(9):
+                    ElementsDG[iElement,13+i] = DG[i].getSubset(region=Element).values[0].data
 
             DGList.append(ElementsDG)
             
@@ -80,7 +82,7 @@ sys.stdout.flush()
 
 with open(sys.argv[1][:-4] + '_DG.csv', 'w') as File:
     Writer = csv.writer(File)
-    Writer.writerow(['Step','Frame', 'X', 'Y', 'Z',
+    Writer.writerow(['Step','Frame', 'N1', 'N2','N3','N4', 'N5', 'N6','N7','N8', 'X', 'Y', 'Z',
                      'F11','F12','F13','F21','F22','F23','F31','F32','F33'])  
     for Frame in DGList:
         for Row in Frame:
